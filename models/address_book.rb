fbbd6c904aaf51fa3dbd987a6c2f6dbe2ca0bc46
mike_stamb <mikestambulyan@gmail.com>
2015-12-21 21:26:41 +0000

    fixed import_csv error

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
-require_relative 'entry'
+require_relative 'entry.rb'
+require "csv"
  
 
  class AddressBook
@@ -6,20 +7,31 @@ require_relative 'entry'
     
     def initialize
      @entries = []
-   end
+    end
    
    
-   def add_entry(name, phone_number, email)
+    def add_entry(name, phone_number, email)
        
        index = 0
      @entries.each do |entry|
          
          if name < entry.name
          break
-       end
+    end
        index += 1
-     end
+    end
      
       @entries.insert(index, Entry.new(name, phone_number, email))
-   end
+    end
+   
+    def import_from_csv(file_name)
+      csv_text = File.read(file_name)
+     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+     
+     csv.each do |row|
+       row_hash = row.to_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     end
+    
+    end
  end
